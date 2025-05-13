be0fd4218dc55f8d033c569431e4fc9bf1c9a127
Aman2104 <amanyara88@gmail.com>
2025-05-13 19:36:25 +0530

    Review All Warning

--- app/src/main/java/com/aman/cricmate/model/Constants.kt
+++ app/src/main/java/com/aman/cricmate/model/Constants.kt
@@ -1,23 +1,22 @@
 package com.aman.cricmate.model
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.ThumbUp
 
 object Constants {
-    const val serverUrl= ""
+    const val serverUrl = ""
     val HomeBottomNavItems = listOf(
         BottomNavItem("home", "Home", Icons.Filled.Home),
-        BottomNavItem("data", "Data", Icons.Filled.List),
+        BottomNavItem("data", "Data", Icons.AutoMirrored.Filled.List),
         BottomNavItem("event", "Event", Icons.Filled.DateRange),
         BottomNavItem("review", "Review", Icons.Filled.ThumbUp)
     )
     val CoachBottomNavItems = listOf(
         BottomNavItem("home", "Home", Icons.Filled.Home),
-        BottomNavItem("data", "Data", Icons.Filled.List),
+        BottomNavItem("data", "Data", Icons.AutoMirrored.Filled.List),
     )
     val testItems = listOf(
         "100 Meter Race" to "hundredMeterTime",
